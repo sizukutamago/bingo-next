8d475d5e3a88243c5008dcd4c9e5dace11752c18
sizukutamago <sizukutamago@gmail.com>
2022-12-20 01:17:24 +0900

    fix vitest

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,8 +1,8 @@
+/// <reference types="vitest" />
 import { fileURLToPath } from "url";
 import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
